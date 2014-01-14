2848cb7fd047144cbe2b6cd904583808d80a603e
ewheeler <evanmwheeler@gmail.com>
2014-01-14 14:41:07 +0300

    remove last bit of old contact extension

--- groups/migrations/0001_initial.py
+++ groups/migrations/0001_initial.py
@@ -43,17 +43,6 @@ class Migration(SchemaMigration):
             'is_editable': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64'})
         },
-        'rapidsms.contact': {
-            'Meta': {'object_name': 'Contact'},
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'language': ('django.db.models.fields.CharField', [], {'max_length': '6', 'blank': 'True'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
-            'phone': ('django.db.models.fields.CharField', [], {'max_length': '32', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'})
-        }
     }
 
     complete_apps = ['groups']
